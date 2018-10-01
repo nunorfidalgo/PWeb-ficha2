4a8f9491c529e3d60f4fabecd17ea7a4e8970c2b
Nuno Fidalgo <nunorfidalgo@gmail.com>
2018-10-01 22:11:25 +0100

    exercicios aula3

---
+++ ex3_Abstract+.cs/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+abstract partial class Forma
+{
+    public abstract double Area();
+    public abstract double Perimetro();
+    public abstract void Detalhe(string formato);
+}
+
+
+//*********************************
+//CLASSE RETANGULO
+
+partial class Retangulo : Forma
+{
+    public double comprimento { get; set; }
+    public double largura { get; set; }
+
+
+    public Retangulo(double comp = 0, double larg = 0)
+    {
+        comprimento = comp;
+        largura = larg;
+    }
+
+    public override double Area()
+    {
+        return comprimento * largura;
+    }
+
+    public override double Perimetro()
+    {
+        return 2 * comprimento + 2 * largura;
+    }
+
+    public override void Detalhe(string formato)
+    {
+        Console.WriteLine("\n{0}: Comrimento= {1} Largura= {2}", GetType(), comprimento.ToString(formato), largura.ToString(formato));
+        Console.WriteLine("Area= {0} Perimetro= {1}", Area().ToString(formato), Perimetro().ToString(formato));
+    }
+}
+
+//*********************************
+//CLASSE TRIANGULO
+
+partial class Triangulo : Forma
+{
+    public double Plado { get; set; }
+    public double Slado { get; set; }
+    public double Tlado { get; set; }
+
+    public Triangulo(double Plado, double Slado, double Tlado)
+    {
+        this.Plado = Plado; this.Slado = Slado; this.Tlado = Tlado;
+    }
+
+    public override double Area()
+    {
+        double sp = Perimetro() / 2;
+        return Math.Sqrt(sp * (sp - Plado) * (sp - Slado) * (sp - Tlado));
+    }
+
+    public override double Perimetro()
+    {
+        return Plado + Slado + Tlado;
+    }
+
+    public override void Detalhe(string formato)
+    {
+        Console.WriteLine("\n{0}: Lados= {1} | {2} | {3}", GetType(), Plado.ToString(formato), Slado.ToString(formato), Tlado.ToString(formato));
+        Console.WriteLine("Area= {0} Perimetro= {1}", Area().ToString(formato), Perimetro().ToString(formato));
+    }
+}
+
+
+//*********************************
+//CLASSE CIRCULO
+
+partial class Circulo : Forma
+{
+    public double raio { get; set; }
+
+    public Circulo(double r = 0)
+    {
+        raio = r;
+    }
+
+    public override double Area()
+    {
+        return Math.Pow(raio, 2) * Math.PI;
+    }
+
+
+    public override double Perimetro()
+    {
+        return 2 * Math.PI * raio;
+    }
+
+    public override void Detalhe(string formato)
+    {
+        Console.WriteLine("\n{0}: Raio= {1}", GetType(), raio.ToString(formato));
+        Console.WriteLine("Area= {0} Perimetro= {1}", Area().ToString(formato), Perimetro().ToString(formato));
+    }
+
+}
+
+//*********************************
+//MAIN
+
+class Program
+{
+    static void Main()
+    {
+        //Mock data 1
+        Retangulo r1 = new Retangulo(5, 6);
+        r1.Detalhe("F1");
+
+        Forma r2 = new Retangulo(5, 6);
+        r2.Detalhe("F1");
+
+
+        Triangulo t1 = new Triangulo(3, 4, 5);
+        t1.Detalhe("F2");
+
+        Forma t2 = new Triangulo(3, 4, 5);
+        t2.Detalhe("F2");
+
+
+        Circulo c1 = new Circulo() { raio = 7.5 };
+        c1.Detalhe("F3");
+
+        Forma c2 = new Circulo() { raio = 7.5 };
+        c2.Detalhe("F3");
+
+        Console.WriteLine();
+    }
+
+}
+
+
---
+++ ex3_Override+/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace F2Ex3f
+{
+
+    abstract class Quadrilatero
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+
+        abstract public double Area();
+        abstract public double Perimetro();
+    }
+
+    class Retangulo : Quadrilatero
+    {
+        public Retangulo() { }
+
+        public Retangulo(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override double Area() { return x * y; }
+        public override double Perimetro() { return (2 * x + 2 * y); }
+    }
+
+
+
+    //class Quadrado : Retangulo
+    //{
+    //    // Caso 1: 
+    //    public Quadrado() { }
+    //    public Quadrado(double x) { this.x = x; }
+
+    //    //public override double Area() { return x * x; }
+    //    //public override double Perimetro() { return 4 * x; }
+
+    //    public new double Area() { return x * x; }
+    //    public new double Perimetro() { return 4 * x; }
+    //}
+
+    //class Quadrado : Retangulo
+    //{
+    //    //Caso 2:
+    //    public Quadrado() : base() { }
+    //    public Quadrado(double x) : base(x, 0) { }
+
+    //    public override double Area() { return x * x; }
+    //    public override double Perimetro() { return 4 * x; }
+    //}
+
+    class Quadrado : Retangulo
+    {
+        //Caso 3:
+        public Quadrado() : base() { }
+        public Quadrado(double x) : base(x, x) { }
+
+        public override double Area() { return x * x; }
+        public override double Perimetro() { return 4 * x; }
+    }
+
+    //class Quadrado : Retangulo
+    //{
+    //    //Caso 4:
+    //    public Quadrado() : base() { }
+    //    public Quadrado(double x) : base(x, 0) { }
+
+    //    public override double Area() { return base.Area(); }
+    //    public override double Perimetro() { return base.Perimetro(); }
+    //}
+
+    //class Quadrado : Retangulo
+    //{
+    //Caso 5:
+    //public Quadrado() { this.y = 0; }
+    //public Quadrado(double x) { this.x = x; }
+
+    //public new double Area() { y = x; double area = base.Area(); y = 0; return area; }
+    //public new double Perimetro() { y = x; double perimetro = base.Perimetro(); y = 0; return perimetro; }
+    //}
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo rt = new Retangulo { x = 2, y = 1 };
+            Retangulo rs = new Quadrado { x = 2, y = 1 };
+            Quadrado sq = new Quadrado { x = 2, y = 1 };
+
+            Console.WriteLine("\nRectangle: x= {0} y= {1}", rt.x, rt.y);
+            Console.WriteLine("Rectangle[Square]: x= {0} y= {1}", rs.x, rs.y);
+            Console.WriteLine("Square: x= {0} y= {1}\n", sq.x, sq.y);
+
+            Console.WriteLine("\nArea: Rectangle= {0} Rectangle[square] = {1} and Square= {2}", rt.Area(), rs.Area(), sq.Area());
+            Console.WriteLine("Perimetro: Rectangle= {0} Rectangle[square] = {1} and Square= {2}", rt.Perimetro(), rs.Perimetro(), sq.Perimetro());
+
+            Retangulo rt2 = new Retangulo(2, 1);
+            Retangulo rs2 = new Quadrado(2);
+            Quadrado sq2 = new Quadrado(2);
+
+            Console.WriteLine("\nRectangle: x= {0} y= {1}", rt2.x, rt2.y);
+            Console.WriteLine("Rectangle[Square]: x= {0} y= {1}", rs2.x, rs2.y);
+            Console.WriteLine("Square: x= {0} y= {1}\n", sq2.x, sq2.y);
+
+            Console.WriteLine("\nArea: Rectangle= {0} Rectangle[square] = {1} and Square= {2}", rt2.Area(), rs2.Area(), sq2.Area());
+            Console.WriteLine("Perimetro: Rectangle= {0} Rectangle[square] = {1} and Square= {2}", rt2.Perimetro(), rs2.Perimetro(), sq2.Perimetro());
+
+
+            return;
+        }
+    }
+}
+
+
+
